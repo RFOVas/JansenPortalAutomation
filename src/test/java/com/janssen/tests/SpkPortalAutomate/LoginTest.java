d25489b1e036718c45e518f76e81f0aa301166d1
mary <b.vasanthy@gmail.com>
2017-11-11 10:37:47 -0500

    test

--- src/test/java/com/janssen/tests/SpkPortalAutomate/LoginTest.java
+++ src/test/java/com/janssen/tests/SpkPortalAutomate/LoginTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 /**
- * Mary 11/08/2017
+ * Mary 11/09/2017s
  */
 public class LoginTest {
 
@@ -43,6 +43,38 @@ public class LoginTest {
 		Logout.click();
 	}
 
+	@Test
+	public void testLoginToFacebook() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MBoreddy\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://janssenspeakerportal2.d2clients.com/login");
+		WebElement emailInputLocator = driver.findElement(By.cssSelector("#email"));
+		WebElement passwordInputLocator = driver.findElement(By.cssSelector("#password"));
+		WebElement submitLogInButtonLocator = driver.findElement(By.cssSelector("#loginButton"));
+
+		System.out.println("email locator     " + emailInputLocator);
+		System.out.println("password locator     " + passwordInputLocator);
+
+		emailInputLocator.sendKeys("Testmgarc204@ITS.JNJ.com");
+		passwordInputLocator.sendKeys("Password1!");
+		submitLogInButtonLocator.click();
+
+		Thread.sleep(2000);
+		WebElement brandsDropDown = driver.findElement(By.cssSelector("#brandsDropDown"));
+		brandsDropDown.click();
+		Thread.sleep(1000);
+		// brandsDropDown.visible() = 'Y'
+		Select dropdown = new Select(brandsDropDown);
+		dropdown.selectByIndex(1);
+
+		driver.findElement(By.xpath(".//*[@id='dLabel']")).click();
+		Thread.sleep(1000);
+
+		WebElement Logout = driver.findElement(By.linkText("Logout"));
+		Logout.click();
+	}
+
+	
 	@Test
 	public void exampleOfTestNgMaven() {
 		System.out.println("Mary This is TestNG-Maven Example");
