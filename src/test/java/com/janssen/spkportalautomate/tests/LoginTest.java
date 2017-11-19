dcd98bd6f035303cbd16dfd20c59163704fc2f13
mary <b.vasanthy@gmail.com>
2017-11-19 10:20:51 -0500

    Initial Commit

--- src/test/java/com/janssen/spkportalautomate/tests/LoginTest.java
+++ src/test/java/com/janssen/spkportalautomate/tests/LoginTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /**
@@ -15,9 +17,21 @@ public class LoginTest {
 	WebDriver driver;
 
 	@Test(priority = 1)
-	public void testLogin() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MBoreddy\\chromedriver.exe");
-		driver = new ChromeDriver();
+	@Parameters("browser_name")
+	public void testLogin(String browserName) throws Exception {
+		if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\MBoreddy\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			System.out.println("firefox");
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\MBoreddy\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("ie")) {
+			System.out.println("ie");
+		} else {
+			System.out.println("no broswer selelected");
+			System.exit(1);
+		}
 		driver.get("https://janssenspeakerportal2.d2clients.com/login");
 		WebElement emailInputLocator = driver.findElement(By.cssSelector("#email"));
 		WebElement passwordInputLocator = driver.findElement(By.cssSelector("#password"));
